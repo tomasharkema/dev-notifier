9c27644db9ce079024f19e3751aea3f72790fc64
tomasharkema <tomas@harkema.io>
2025-04-01 11:34:04 +0200

    send notification

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/tomasharkema/dev-notifier
 go 1.24.1
 
 require (
+	github.com/alecthomas/kingpin/v2 v2.4.0 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/esiqveland/notify v0.13.3 // indirect
+	github.com/godbus/dbus v4.1.0+incompatible // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/jkeiser/iter v0.0.0-20200628201005-c8aa0ae784d1 // indirect
 	github.com/jochenvg/go-udev v0.0.0-20240801134859-b65ed646224b // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,34 +3,114 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
+	"os"
+	"os/signal"
 
+	kingpin "github.com/alecthomas/kingpin/v2"
+	"github.com/esiqveland/notify"
+	"github.com/godbus/dbus/v5"
 	udev "github.com/jochenvg/go-udev"
 )
 
+var (
+	follow = kingpin.Flag("follow", "Watch mode").Short('f').Bool()
+)
+
 func main() {
+	root := context.Background()
+
+	ctx, stop := signal.NotifyContext(root, os.Kill, os.Interrupt)
+	defer stop()
+
+	kingpin.Parse()
+
+	if *follow {
+		runFollow(ctx)
+	}
+}
+
+func runFollow(ctx context.Context) {
+	fmt.Println("Follow")
 	u := udev.Udev{}
 	m := u.NewMonitorFromNetlink("udev")
 
-	// Create a context
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var wg sync.WaitGroup
-	wg.Add(3)
-
-	// Start monitor goroutine and get receive channel
 	ch, _, err := m.DeviceChan(ctx)
 	if err != nil {
 		panic(err)
 	}
+
+	conn, err := dbus.SessionBusPrivate()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	if err = conn.Auth(nil); err != nil {
+		panic(err)
+	}
+
+	if err = conn.Hello(); err != nil {
+		panic(err)
+	}
+
 	go func() {
 		fmt.Println("Started listening on channel")
-		for d := range ch {
-			fmt.Println("Event:", d.Syspath(), d.Action())
+		for device := range ch {
+			handleDevice(conn, device)
 		}
-		wg.Done()
 	}()
-	wg.Wait()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("done")
+	}
 	fmt.Println("Channel closed")
+
+}
+
+func handleDevice(conn *dbus.Conn, device *udev.Device) {
+	action := device.Action()
+	devnode := device.Devnode()
+
+	name := device.Properties()["ID_MODEL_FROM_DATABASE"]
+
+	fmt.Println("\n\n=== New event: ===")
+	fmt.Println("Event:", action, device.Syspath())
+	fmt.Println(devnode)
+
+	if action == "add" && devnode != "" && name != "" {
+
+		message := fmt.Sprintf("%s\n%s", name, devnode)
+
+		fmt.Println("Send message:", message)
+
+		n := notify.Notification{
+			AppName: "Device Notification",
+			// ReplacesID: uint32(0),
+			// AppIcon:    iconName,
+			Summary: "Device Added",
+			Body:    message,
+			// Actions: []notify.Action{
+			//   {Key: "cancel", Label: "Cancel"},
+			//   {Key: "open", Label: "Open"},
+			// },
+			// Hints: map[string]dbus.Variant{
+			//   soundHint.ID: soundHint.Variant,
+			// },
+			// ExpireTimeout: time.Second * 5,
+		}
+
+		notifier, err := notify.New(
+			conn,
+		)
+		if err != nil {
+			fmt.Println("error sending notification:", err)
+		}
+
+		id, err := notifier.SendNotification(n)
+		if err != nil {
+			fmt.Println("error sending notification:", err)
+		}
+		fmt.Println("id", id)
+	}
 }
